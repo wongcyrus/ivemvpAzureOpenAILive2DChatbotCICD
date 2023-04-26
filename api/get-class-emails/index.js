2d8c937aed0a5de3ac172eb59bd9061c9855f69b
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-04-26 05:05:45 +0000

    get ip

--- api/get-class-emails/index.js
+++ api/get-class-emails/index.js
@@ -9,7 +9,7 @@ const classesTableClient = TableClient.fromConnectionString(chatStorageAccountCo
 
 module.exports = async function (context, req) {
 
-    context.log(req.headers);
+    context.log("ip:" + request.getHeader('header-name'));
     const teacherEmail = getEmail(req);
 
     if (!await isTeacher(teacherEmail, context)) {
