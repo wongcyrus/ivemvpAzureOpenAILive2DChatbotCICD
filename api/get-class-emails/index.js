b47cc49e7d33cf57fde68e8c934c028e4ffe2ef3
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-04-26 05:15:29 +0000

    x-forwarded-for

--- api/get-class-emails/index.js
+++ api/get-class-emails/index.js
@@ -9,7 +9,7 @@ const classesTableClient = TableClient.fromConnectionString(chatStorageAccountCo
 
 module.exports = async function (context, req) {
 
-    context.log("ip:" + req.getHeader('header-name'));
+    context.log("ip:" + req.getHeader('x-forwarded-for'));
     const teacherEmail = getEmail(req);
 
     if (!await isTeacher(teacherEmail, context)) {
