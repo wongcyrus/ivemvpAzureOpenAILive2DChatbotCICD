9c1848a454f8af2ee110c2c4056acf9fc02d7ab2
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-04-26 03:19:17 +0000

    add get-class-emails

---
+++ api/get-class-emails/index.js
@@ -0,0 +1,45 @@
+const { TableClient } = require("@azure/data-tables");
+const { BlobServiceClient, BlobSASPermissions, SASProtocol } = require("@azure/storage-blob");
+const { getEmail, isTeacher } = require("../checkMember");
+const { setJson, setErrorJson } = require("../contextHelper");
+
+
+const chatStorageAccountConnectionString = process.env.chatStorageAccountConnectionString;
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(chatStorageAccountConnectionString);
+const containerClient = blobServiceClient.getContainerClient("screen");
+const classesTableClient = TableClient.fromConnectionString(chatStorageAccountConnectionString, "classes");
+
+
+module.exports = async function (context, req) {
+
+    context.log(req["header"]);
+    const teacherEmail = getEmail(req);
+
+    if (!await isTeacher(teacherEmail, context)) {
+        setErrorJson(context, "Unauthorized", 401);
+        return;
+    }
+
+    const classId = req.query.classId;
+
+    let continuationToken = null;
+    let pageEntities = undefined;
+    let entities = [];
+    do {
+        const page = await classesTableClient.listEntities({
+            queryOptions: {
+                filter: `PartitionKey eq '${classId}'`
+            }
+        }).byPage({ maxPageSize: 100, continuationToken: continuationToken }).next();
+        pageEntities = page.value;
+        continuationToken = pageEntities.continuationToken;
+        entities = entities.concat(pageEntities);
+    }
+    while (continuationToken !== undefined);
+    let students = entities.map(entity => ({ email: entity.rowKey, name: entity.Name }));
+    students.sort((p1, p2) => p1.email.localeCompare(p2.email))
+
+    setJson(context, students);
+
+}
